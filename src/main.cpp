0b55fd0a8c65abd0171f721ad2574c06b89be636
Kaitlyn <49522306+ItsKaitlyn@users.noreply.github.com>
2022-09-01 09:25:47 -0400

    remove unused stuff lol

--- src/main.cpp
+++ src/main.cpp
@@ -28,26 +28,6 @@ void ServerDisconnect()
 		KillClient();
 }
 
-//0x40AE30
-void DefaultConfigData(CS_ConfigData *conf)
-{
-	//Claer old config data
-	memset(conf, 0, sizeof(CS_ConfigData));
-
-	conf->window_size = 2;
-
-	//Reset joystick settings (as these can't simply be set to 0)
-	conf->gamepad_enabled = 1;
-	conf->gamepad_buttons[0] = 2;
-	conf->gamepad_buttons[1] = 1;
-	conf->gamepad_buttons[2] = 5;
-	conf->gamepad_buttons[3] = 6;
-	conf->gamepad_buttons[4] = 3;
-	conf->gamepad_buttons[5] = 4;
-	conf->gamepad_buttons[6] = 6;
-	conf->gamepad_buttons[7] = 3;
-}
-
 int frame_x = 0;
 int frame_y = 0;
 
@@ -56,28 +36,6 @@ void InactiveWindow()
 {
 	// nothing lol
 }
-
-// Idk why I do this here tbh
-
-/*
-static void SetWindowName(HWND hWnd)
-{
-	CS_MakeSurface_Generic(CS_window_surface_width * 2, MAX_CLIENTS * 16, CS_SURFACE_ID_UNKNOWN_12);
-	// CS_MakeSurface_Generic(CS_window_surface_width * 2, MAX_CLIENTS * 16, CS_SURFACE_ID_UNKNOWN_4);
-	char* window_name;
-	window_name = "CS Freeware Online";
-	SetWindowTextA(hWnd, window_name);
-}
-*/
-
-
-// Handles the connection instead of whimsical star lol
-/*
-void ActStar()
-{
-
-}
-*/
 
 int networkStarted = 0;
 RECT rcDrownedChar = { 32, 80, 48, 96 };
@@ -153,8 +111,6 @@ void InitMod(void)
 	mim_compatibility = ModLoader_GetSettingInt("MIM_Compatibility", 0);
 	show_player_names = ModLoader_GetSettingInt("Player Names", 0);
 
-	ModLoader_WriteJump((void*)0x40AE30, (void*)DefaultConfigData);
-	// ModLoader_WriteJump((void*)0x412320, (void*)SetWindowName);
 	ModLoader_WriteJump((void*)0x412BC0, (void*)InactiveWindow);
 	
 	//Hooking into the end of PutBullet
